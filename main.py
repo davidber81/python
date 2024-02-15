144c572e0d7a84a7ed444ac2d38d1b817ba22500
user <davidber81@mail.ru>
2024-02-15 17:55:56 +0300

    Module

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def test_function():
-    def inner_function():
-        print("Я в области видимости функции test_function")
-    inner_function()
+class Vehicle:
+    """ Транспортное средство """
 
-test_function()
+    def __init__(self, model, vehicle_type):
+        self.model = model
+        self.vehicle_type = vehicle_type
+    def horse_powers(self):
+        return 300
+
+    def __str__(self):
+        return 'model {} , марка {}, мощность {}' .format(self.__class__.__name__, self.vehicle_type, horse_powers)
+
+class Car:
+    """ Машина """
+    price = 200000
+
+    def horse_powers(self):
+        return 200
+
+
+class Nissan(Vehicle, Car):
+    vehicle_type = 'z51'
+    price = 1000000
+
+    def horse_powers(self):
+       return 120
+
+" Вывод "
+my_car = Nissan('model', 'z51')
+vehicle = my_car.vehicle_type
+price = my_car.price
+horse_powers = my_car.horse_powers()
+print(my_car)
+print(vehicle)
+print(price)
+print(horse_powers)
